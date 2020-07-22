6eaa704786ee2a89f872e8357e9c375472bdf9ec
chris pryer <christophpryer@gmail.com>
2020-07-22 18:54:37 -0400

    updated for basic http server

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,35 @@
-use std::net::TcpListener;
+extern crate hyper;
+extern crate futures;
 
-fn main() {
-    let listener = TcpListener::bind("0.0.0.0:5000").unwrap();
+#[macro_use]
+extern crate log;
+extern crate env_logger;
 
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
+use hyper::server::{Request, Response, Service};
 
-        println!("Connection established!");
+use futures::future::Future;
+
+struct Microservice;
+
+impl Service for Microservice {
+    type Request = Request;
+    type Response = Response;
+    type Error = hyper::Error;
+    type Future = Box<Future<Item = Self::Response, Error = Self::Error>>;
+
+    fn call(&self, request: Request) -> Self::Future {
+        info!("Microservice received a request: {:?}", request);
+        Box::new(futures::future::ok(Response::new()))
     }
 }
+
+fn main() {
+    env_logger::init();
+    let address = "127.0.0.1:7878".parse().unwrap();
+    let server = hyper::server::Http::new()
+        .bind(&address, || Ok(Microservice {}))
+        .unwrap();
+    info!("Running microservice at {}", address);
+    server.run().unwrap();
+}
+
